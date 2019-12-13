1733f304bf5f3943c3d19074a4a6ac376c433213
Febiola Agatha <febiolagatha@gmail.com>
2019-12-13 17:10:05 +0700

    menu utama

---
+++ laporci/application/views/halamanutama.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="halamanutama.css">
+    <meta charset="utf-8">
+    <title>Buat Laporan</title>
+  </head>
+
+  <body>
+    <div class="judul">
+      <h1>SIMPLE LAPOR!</h1>
+    </div>
+  <br>
+  <br>
+    <div class="pencarian">
+        <form method="POST" action="" enctype="multipart/form-data">
+          <textarea name="search" rows="3" cols="132"></textarea>
+          <button type="submitSearch" name="tombol" class="searchButton" rows="3" cols="7"> Cari </button>
+
+    </div>
+  <br>
+    <div class="buatlaporan">
+      <form method="post" action="buatlaporan.php">
+      </form> <button type="createLapor" class="lapor">Buat laporan/komentar<i class="fa fa-plus"></i>
+      </button>
+    </div>
+  <br>
+  <br>
+    <div class="tampilandata">
+      <p>Laporan/Komentar Terakhir</p>
+    </div>
+
+  </body>
+  </html>
